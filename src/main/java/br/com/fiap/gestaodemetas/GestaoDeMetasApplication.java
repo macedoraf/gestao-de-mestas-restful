0c2b6bf41bbcf4557225b3db21fa301287bff56d
Rafael Macedo <macedo.rafanazareth@gmail.com>
2021-10-10 11:51:17 -0300

    service

--- src/main/java/br/com/fiap/gestaodemetas/GestaoDeMetasApplication.java
+++ src/main/java/br/com/fiap/gestaodemetas/GestaoDeMetasApplication.java
@@ -3,10 +3,12 @@ package br.com.fiap.gestaodemetas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("br.com.fiap.gestaodemetas.repository")
+@ComponentScan("br.com.fiap.gestaodemetas.service")
 public class GestaoDeMetasApplication {
 
 	public static void main(String[] args) {
